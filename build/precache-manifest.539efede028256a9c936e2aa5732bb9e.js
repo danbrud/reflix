2955e4d5d29fd7d57539698e424807939289cbde
Danny <dannybrudner@gmail.com>
2019-05-21 13:51:17 +0300

    Ran build

---
+++ build/precache-manifest.539efede028256a9c936e2aa5732bb9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a28958ae7cd447f314a56d9c1f6b15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7cf7deba0a2fa9648ca",
+    "url": "/static/css/main.103bd366.chunk.css"
+  },
+  {
+    "revision": "46fafe9218c73bdb2e09",
+    "url": "/static/js/2.39dcef0c.chunk.js"
+  },
+  {
+    "revision": "a7cf7deba0a2fa9648ca",
+    "url": "/static/js/main.50fc4792.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
